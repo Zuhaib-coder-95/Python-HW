859a2710d343a56e09089c14943747a0937ed1be
Zuhaib-coder-95 <ishaqsyd.gm@gmail.com>
2025-12-21 15:12:23 +0400

    Add files via upload

---
+++ Storingvariables.py
@@ -0,0 +1,21 @@
+#Storing birthday values in variables
+person1 = "Alex"
+birthday1 = "January 5"
+
+person2 = "David"
+birthday2 = "March 12"
+
+person3 = "John"
+birthday3 = "June 20"
+
+person4 = "Harry"
+birthday4 = "August 9"
+
+person5 = "Daniel"
+birthday5 = "November 30"
+
+print(person1, "birthday is", birthday1)
+print(person2, "birthday is", birthday2)
+print(person3, "birthday is", birthday3)
+print(person4, "birthday is", birthday4)
+print(person5, "birthday is", birthday5)
