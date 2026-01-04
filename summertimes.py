017928d97d02061e7368f64e9d0e429e82dfa974
Zuhaib-coder-95 <ishaqsyd.gm@gmail.com>
2026-01-04 15:15:44 +0400

    Add files via upload

---
+++ summertimes.py
@@ -0,0 +1,6 @@
+temperature = float(input("Enter the current temperature: "))
+
+if temperature >= 20:
+    print("It's suitable to wear light and soft clothes.")
+else:
+    print("It's too cold. You should wear your jacket and pullover.")
