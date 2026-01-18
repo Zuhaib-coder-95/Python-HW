fa2bbe317a805c061006bbd86362e9787385ca0d
Zuhaib-coder-95 <ishaqsyd.gm@gmail.com>
2026-01-18 15:26:13 +0400

    Add files via upload

---
+++ check_age.py
@@ -0,0 +1,6 @@
+age = int(input("Enter student's age: "))
+
+if age >= 10 and age <= 20:
+    print("You are allowed to enroll in the class.")
+else:
+    print("You are not allowed to enroll in the class.")
